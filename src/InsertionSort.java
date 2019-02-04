303dd238a647569191d0a93bddaf7f2ab571a029
kosten27 <kosten27@gmail.com>
2019-02-04 22:56:50 +0200

    Insertion sort

---
+++ src/InsertionSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] ints = {8, 2, 5, 3, 4, 6, 1};
+
+        sort(ints);
+    }
+
+    public static void sort(int[] ints) {
+
+        int step = 0;
+        for (int i = 1; i < ints.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (ints[i] < ints[j]) {
+                    step++;
+                    int a = ints[j];
+                    ints[j] = ints[i];
+                    ints[i] = a;
+                    System.out.printf("step %2d: " + Arrays.toString(ints), step);
+                    System.out.println();
+                 }
+            }
+        }
+        System.out.println("result:  " + Arrays.toString(ints));
+    }
+}
