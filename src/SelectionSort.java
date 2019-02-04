fcee6d8e3c99a27705b44ca40403b65d35ba3923
kosten27 <kosten27@gmail.com>
2019-02-04 23:01:58 +0200

    Selection sort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -9,19 +9,23 @@ public class SelectionSort {
 
     public static void sort(int[] ints) {
 
+        int step = 0;
         for (int i = 0; i < ints.length; i++) {
             int min_idx = i;
             for (int j = i; j < ints.length; j++) {
                 if (ints[min_idx] > ints[j]) {
                     min_idx = j;
                 }
-                if (!(min_idx == i)) {
-                    int a = ints[i];
-                    ints[i] = ints[min_idx];
-                    ints[min_idx] = a;
-                }
+            }
+            if (!(min_idx == i)) {
+                step++;
+                int a = ints[i];
+                ints[i] = ints[min_idx];
+                ints[min_idx] = a;
+                System.out.printf("step %2d: " + Arrays.toString(ints), step);
+                System.out.println();
             }
         }
-        System.out.println(Arrays.toString(ints));
+        System.out.println("result:  " + Arrays.toString(ints));
     }
 }
