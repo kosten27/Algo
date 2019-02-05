ff5c62190bd959a1d9e053cd4e936bb51b8b76ce
kosten27 <kosten27@gmail.com>
2019-02-05 23:47:40 +0200

    Merge sort

---
+++ src/MergeSort.java
@@ -0,0 +1,11 @@
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] ints = {8, 3, 5, 0, 4, 6, 1, 2, 7};
+
+        sort(ints);
+    }
+
+    public static void sort(int[] ints) {
+
+    }
+}
