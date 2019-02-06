215ee510de3b44d7362126bb5f8257edfe20edb7
kosten27 <kosten27@gmail.com>
2019-02-07 00:00:11 +0200

    Merge branch

---
+++ src/BubbleSort.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] ints = {8, 2, 5, 3, 4, 6, 1};
+
+        sort(ints);
+    }
+
+    public static void sort(int[] ints) {
+
+        int step = 0;
+        for (int i = 0; i < ints.length; i++) {
+            int count = 0;
+            for (int j = 0; j < ints.length - i - 1; j++) {
+                if (ints[j] > ints[j + 1]) {
+                    step++;
+                    count++;
+                    int a = ints[j];
+                    ints[j] = ints[j + 1];
+                    ints[j + 1] = a;
+                    System.out.printf("step %2d: " + Arrays.toString(ints), step);
+                    System.out.println();
+                }
+            }
+            if (count == 0) {
+                break;
+            }
+        }
+        System.out.println("result:  " + Arrays.toString(ints));
+    }
+}
